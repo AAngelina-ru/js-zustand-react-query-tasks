4f245f78b7390c7279caaf2ba9fcd39f0a1fa7b1
Ангелина <gagylunaanglina@gmail.com>
2026-09-17 02:12:14 +0300

    Update test.jsx

--- 1/__tests__/test.jsx
+++ 1/__tests__/test.jsx
@@ -27,3 +27,14 @@ test('increment, decrement and reset', async () => {
   await userEvent.click(screen.getByTestId('reset'));
   expect(screen.getByTestId('value')).toHaveTextContent('0');
 });
+const App = () => {
+  const { value, increment, decrement, reset } = useCounterStore();
+  return (
+    <div>
+      <div data-testid="value">{value}</div>
+      <button data-testid="increment" onClick={increment}>+</button>
+      <button data-testid="decrement" onClick={decrement}>-</button>
+      <button data-testid="reset" onClick={reset}>Reset</button>
+    </div>
+  );
+};
