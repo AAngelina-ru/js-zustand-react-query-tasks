35aabcaa69d6c356ebd4da2bd8d74f487b362776
Ангелина <gagylunaanglina@gmail.com>
2026-09-17 02:09:27 +0300

    Update vite.config.js

--- 1/vite.config.js
+++ 1/vite.config.js
@@ -7,3 +7,18 @@ export default defineConfig({
     open: true,
   },
 });
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
