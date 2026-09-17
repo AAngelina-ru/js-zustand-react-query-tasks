621b0490fdb587719c82efc834f50e9891745aef
Ангелина <gagylunaanglina@gmail.com>
2026-09-17 03:27:46 +0300

    Update vite.config.js

--- 3/vite.config.js
+++ 3/vite.config.js
@@ -7,3 +7,22 @@ export default defineConfig({
     open: true,
   },
 });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true,
+    port: 5173, // можно явно задать порт
+  },
+  build: {
+    outDir: 'dist', // папка для сборки
+    sourcemap: true, // удобно для отладки в продакшене
+  },
+  resolve: {
+    alias: {
+      '@': '/src', // алиас для импортов, например import Foo from '@/components/Foo'
+    },
+  },
+})
