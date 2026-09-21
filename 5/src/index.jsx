90ac3209cf08462fa8f5e5ee304674a9c9abeafa
Ангелина <gagylunaanglina@gmail.com>
2026-09-21 17:41:46 +0300

    Update index.jsx

--- 5/src/index.jsx
+++ 5/src/index.jsx
@@ -19,3 +19,11 @@ root.render(
     <App />
   </QueryClientProvider>,
 );
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 2,
+      retryDelay: (attemptIndex) => Math.min(1000 * 2 ** attemptIndex, 10000),
+    },
+  },
+});
